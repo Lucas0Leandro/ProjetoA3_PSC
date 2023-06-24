57ae362ce11f9ea3a6eb426f3093fbbfe1fad50b
playlucas99br <128534280+playlucas99br@users.noreply.github.com>
2023-06-23 23:29:05 -0300

    commit Turmas DAO

---
+++ src/DAO/DAO_Turmas.java
@@ -0,0 +1,36 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Conexao.conexao;
+import EduPack.Turmas;
+
+public class DAO_Turmas {
+
+    public void cadastrarTurmas(Turmas turmas){
+
+        String sql = "INSERT INTO TURMA (ID, SEMESTRE, ANO, TIPO, HORARIO) VALUES(?, ?, ?, ?, ?)";
+
+        PreparedStatement ps = null;
+        
+            try {
+                ps = conexao.getConexao().prepareStatement(sql);
+                ps.setLong(1, turmas.getID());
+                ps.setLong(2, turmas.getSemestre());
+                ps.setLong(3, turmas.getAno());
+                ps.setLong(4, turmas.getTipo());
+                ps.setTime(5, turmas.getHorario());
+
+
+                ps.execute();
+                ps.close();
+                
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+
+    }
+
+}
