4fba9be5d7b7de82bb02e029022ef0f74b3c63de
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-23 22:02:22 -0300

    commit  TelaProfessor

--- src/PessoaPack/Professor.java
+++ src/PessoaPack/Professor.java
@@ -4,4 +4,12 @@ public class Professor extends Pessoa {
     
     private int idFuncionario;
 
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
 }
