c85b2483f9ffd77ecf84577f95480c870029d18a
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-24 00:15:23 -0300

    last one

--- src/EduPack/Turmas.java
+++ src/EduPack/Turmas.java
@@ -8,7 +8,7 @@ public class Turmas {
     private int semestre;
     private int ano;
     private char tipo;
-    private Time horario;
+    private String horario;
 
     //Usando o get para podemos utilizar em outros package de forma publica
 
@@ -30,6 +30,26 @@ public class Turmas {
     }
     public Time getHorario(){
         return horario;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
     }
 }
 
