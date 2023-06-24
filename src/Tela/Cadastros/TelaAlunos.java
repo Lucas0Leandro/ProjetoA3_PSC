6b0d8c9033e860a9f5474d61b51523ea998ba9e6
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-23 21:49:39 -0300

    Commit FOIIIIII!!!!!!!!!!
    Amem senhor Jesus Cristo, e chatGPT

--- src/Tela/Cadastros/TelaAlunos.java
+++ src/Tela/Cadastros/TelaAlunos.java
@@ -1,53 +1,48 @@
 package Tela.Cadastros;
 
-
 import java.util.Scanner;
 import DAO.DAO_Aluno;
 import PessoaPack.Aluno;
 
 public class TelaAlunos {
-    String nome, sobrenome, CPF, fone, endereco, ra, cep;
-
+    
     public void exibir() throws InterruptedException {
+        String nome, sobrenome, CPF, fone, endereco;
+        int ra, cep;
 
         Scanner teclado = new Scanner(System.in);
 
-            System.out.println("//--------------------//");
-            System.out.println("    CADASTRAR ALUNO");
-            System.out.print("Nome: ");
-            nome = teclado.nextLine();
-            System.out.print("\nSobrenome: ");
-            sobrenome = teclado.nextLine();
-            System.out.print("\nCPF: ");
-            CPF = teclado.nextLine();
-            System.out.print("\nFone: ");
-            fone = teclado.nextLine();
-            System.out.print("\nEndereco: ");
-            endereco = teclado.nextLine();
-            System.out.print("\nCEP: ");
-            cep = teclado.nextLine();
-            System.out.print("\nRA: ");
-            ra = teclado.nextLine();
-            System.out.println("       CONCLUIDO");
-            System.out.println("//--------------------//\n");
-        
+        System.out.println("//--------------------//");
+        System.out.println("    CADASTRAR ALUNO");
+        System.out.print("Nome: ");
+        nome = teclado.nextLine();
+        System.out.print("\nSobrenome: ");
+        sobrenome = teclado.nextLine();
+        System.out.print("\nCPF: ");
+        CPF = teclado.nextLine();
+        System.out.print("\nFone: ");
+        fone = teclado.nextLine();
+        System.out.print("\nEndereco: ");
+        endereco = teclado.nextLine();
+        System.out.print("\nCEP: ");
+        cep = teclado.nextInt();
+        System.out.print("\nRA: ");
+        ra = teclado.nextInt();
+        System.out.println("       CONCLUIDO");
+        System.out.println("//--------------------//\n");
+
+        Aluno aluno = new Aluno();
+        aluno.setRa(ra);
+        aluno.setNome(nome);
+        aluno.setSobrenome(sobrenome);
+        aluno.setCPF(CPF);
+        aluno.setFone(fone);
+        aluno.setEndereço(endereco);
+        aluno.setCep(cep);
+
+        DAO_Aluno daoAluno = new DAO_Aluno();
+        daoAluno.cadastrarAluno(aluno);
     }
-/* 
-    public static void main(String[] args){
-
-        Aluno a = new Aluno();
-    
-        a.setRa(ra);
-        a.setNome(nome);
-        a.setSobrenome(sobrenome);
-        a.setCPF(CPF);
-        a.setFone(fone);
-        a.setEndereço(endereco);
-        a.setCep(cep);
-        
-        new DAO_Aluno().cadastrarAluno(a);
-
-    }*/
 
     public static void clearTerminal() {
         System.out.print("\033[H\033[2J");
