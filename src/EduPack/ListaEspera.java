40c90f6317b0e7752c7bc1a5998819471dd7715b
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-26 20:47:37 -0300

    commit get set

--- src/EduPack/ListaEspera.java
+++ src/EduPack/ListaEspera.java
@@ -5,5 +5,31 @@ public class ListaEspera {
     private int ID;
     private String alunos;
     private String turmas;
+
+    
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getAlunos() {
+        return alunos;
+    }
+    public void setAlunos(String alunos) {
+        this.alunos = alunos;
+    }
+    public String getTurmas() {
+        return turmas;
+    }
+    public void setTurmas(String turmas) {
+        this.turmas = turmas;
+    }
+    public String getMatricula() {
+        return Matricula;
+    }
+    public void setMatricula(String matricula) {
+        Matricula = matricula;
+    }
     private String Matricula; 
 }
--- src/MatriculaPack/Matricula.java
+++ src/MatriculaPack/Matricula.java
@@ -8,4 +8,30 @@ public class Matricula {
     private Date data;
     private boolean validada;
     private Date expira;
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public boolean isValidada() {
+        return validada;
+    }
+    public void setValidada(boolean validada) {
+        this.validada = validada;
+    }
+    public Date getExpira() {
+        return expira;
+    }
+    public void setExpira(Date expira) {
+        this.expira = expira;
+    }
 }
