e48f721aed1a046b35883ee7eeeee52195c7cda2
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-27 00:36:07 -0300

    commit conexao
    atualizei a classe ConexaoBD

--- src/Conexao/ConexaoBD.java
+++ src/Conexao/ConexaoBD.java
@@ -5,15 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexao {
-    
-    private static final String url = "jdbc:mysql://localhost:3306/escola";
-    private static final String user = "root";
-    private static final String password = "1234";
 
+    private static conexao instance = null;
+    private static String url = "jdbc:mysql://localhost:3306/escola";
+    private static String user = "root";
+    private static String password = "1234";
     private static Connection conn;
-    
-    public static Connection getConexao(){
 
+    private conexao {
+
+    }
+    
+    public Connection getConexao(){
 
         try {
             if(conn == null){
