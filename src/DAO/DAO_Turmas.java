86a4950420b757228e7cd1ff7fbe3b9bf1ed7d16
playlucas99br <128534280+playlucas99br@users.noreply.github.com>
2023-06-27 03:03:50 -0300

    commit DAO TURMA

--- src/DAO/DAO_Turmas.java
+++ src/DAO/DAO_Turmas.java
@@ -1,7 +1,11 @@
 package DAO;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Conexao.ConexaoBD;
 import EduPack.Turmas;
@@ -30,6 +34,35 @@ public class DAO_Turmas {
                 e.printStackTrace();
         }
 
+    }
+    public List<Turmas> listar() {
+        List<Turmas> listaCursos = new ArrayList<Turmas>();
+        try {
+            Connection connection = ConexaoBD.getConexao(); // Obtém a conexão corretamente
+
+            String sql = "SELECT * FROM TURMAS";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Turmas turmas = new Turmas(0, 0, 0, sql, sql); // Certifique-se de atualizar os argumentos com os tipos corretos
+                turmas.setID(rs.getInt("ID"));
+                turmas.setSemestre(rs.getInt("Semestre"));
+                turmas.setAno(rs.getInt("Ano"));
+                turmas.setHora(rs.getString("Hora"));
+                turmas.setTipo(rs.getString("Tipo"));
+
+                listaCursos.add(turmas);
+            }
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return listaCursos;
     }
 
 }
