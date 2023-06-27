4156b57a773c86afa6fa9d78890d4d82321a5b06
Lucas Leandro <lucasoleandro18@GMAIL.COM>
2023-06-27 03:13:53 -0300

    commit TelaConsultaCursos

--- src/Tela/Consultas/TelaSalas.java
+++ src/Tela/Consultas/TelaSalas.java
@@ -7,15 +7,29 @@ import java.util.List;
 
 public class TelaSalas {
     
-    public void exibir() throws InterruptedException {
-        
+    public void exibir() {
+        int opc;
+        Scanner teclado = new Scanner(System.in);
+        DAO_Salas daoSalas = new DAO_Salas();
 
-        
-    }
+        do {
+            System.out.println("//-------------------------//");
+            System.out.println("       LISTA DE SALAS");        
 
-    public static void clearTerminal() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+            List<Salas> listaSalas = daoSalas.listar();
+
+            for (Salas salas : listaSalas) {
+                System.out.println("\nID: " + salas.getID());
+                System.out.println("Capacidade: " + salas.getCapacidade());
+                System.out.println("Local: " + salas.getLocal());
+                System.out.println("--------------------------------------------------------------------");
+            }
+
+            System.out.println("\n\nDigite 0 para sair.");
+            opc = teclado.nextInt();
+
+        } while (opc != 0);
+        
     }
 
 }
