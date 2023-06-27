30969db8deb47699a2f228f1a418635164c07808
playlucas99br <128534280+playlucas99br@users.noreply.github.com>
2023-06-27 02:49:47 -0300

    commit DAO_CURSOS

--- src/DAO/DAO_Cursos.java
+++ src/DAO/DAO_Cursos.java
@@ -1,7 +1,11 @@
 package DAO;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Conexao.ConexaoBD;
 import EduPack.Cursos;
@@ -30,6 +34,35 @@ public class DAO_Cursos {
                 e.printStackTrace();
         }
 
+    }
+    public List<Cursos> listar() {
+        List<Cursos> listaCursos = new ArrayList<Cursos>();
+        try {
+            Connection connection = ConexaoBD.getConexao(); // Obtém a conexão corretamente
+
+            String sql = "SELECT * FROM CURSOS";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Cursos cursos = new Cursos(0, sql, 0, sql); // Certifique-se de atualizar os argumentos com os tipos corretos
+                cursos.setId(rs.getInt("ID"));
+                cursos.setNome(rs.getString("Nome"));
+                cursos.setCargaHoraria(rs.getInt("CargaHoraria"));
+                cursos.setDescriçãoSobre(rs.getString("descricao"));
+
+
+                listaCursos.add(cursos);
+            }
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return listaCursos;
     }
     
 }
